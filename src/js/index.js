c8e4e4a14de80e7ff88e02ca1793b9ab5dbc5a5f
Lajos.Meszaros <m_lajos@hotmail.com>
2020-09-26 22:30:04 +0200

    Added a test implementation for a simple and a complex gate

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,65 @@
 /* global R */
 
-const { add } = R
+const { init, last, evolve } = R
 
-console.log(add(1, 3))
+const tick = gate => {
+  if (gate.gates) {
+    const { gates, output } = gate.handler(gate.inputs, gate.gates)
+    return evolve(
+      {
+        gates: () => gates,
+        output: () => output
+      },
+      gate
+    )
+  } else {
+    const buffer = [gate.handler(gate.inputs), ...gate.buffer]
+    return evolve(
+      {
+        buffer: () => init(buffer),
+        output: () => last(buffer)
+      },
+      gate
+    )
+  }
+}
+
+const NOR = () => {
+  return {
+    inputs: {
+      a: false,
+      b: false
+    },
+    buffer: [],
+    handler: ({ a, b }) => !a && !b,
+    output: null
+  }
+}
+
+const CLOCK = () => {
+  return {
+    gates: {
+      nor: NOR()
+    },
+    inputs: {},
+    output: null,
+    handler: (inputs, gates) => {
+      gates.nor.inputs.b = gates.nor.output
+      const nextNor = tick(gates.nor)
+
+      return {
+        gates: {
+          nor: nextNor
+        },
+        output: nextNor.output
+      }
+    }
+  }
+}
+
+let clock = CLOCK()
+
+for (let i = 0; i < 30; i++) {
+  clock = tick(clock)
+  console.log(clock.output)
+}
