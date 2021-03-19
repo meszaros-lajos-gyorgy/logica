e2effc327b923091981c3cd1416e942cb109e09c
Mészáros Lajos <m_lajos@hotmail.com>
2021-03-19 20:13:42 +0100

    Az egér lenyomást és pozíciót is nyilvántartom most már

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ const ctx = canvas.getContext("2d");
 const state = {
   dirty: true,
   screen: {
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 0,
+    height: 0,
   },
+  mouse: {
+    pressed: false,
+    posX: 0,
+    posY: 0,
+  },
+};
+
+const renderScene = () => {
+  ctx.fillStyle = "#eaeaea";
+  ctx.fillRect(0, 0, state.screen.width, state.screen.height);
+
+  ctx.fillStyle = "rgb(200, 0, 0)";
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect(30, 30, 50, 50);
+};
+
+const mainLoop = () => {
+  if (state.dirty) {
+    state.dirty = false;
+    renderScene();
+  }
+
+  window.requestAnimationFrame(mainLoop);
 };
 
 const resizeCanvas = () => {
@@ -16,28 +41,34 @@ const resizeCanvas = () => {
   ) {
     state.screen.width = window.innerWidth;
     state.screen.height = window.innerHeight;
+
+    state.dirty = true;
+
+    canvas.width = state.screen.width;
+    canvas.height = state.screen.height;
+  }
+};
+
+window.requestAnimationFrame(mainLoop);
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+canvas.addEventListener("mousedown", (e) => {
+  if (e.button === 0) {
+    state.mouse.pressed = true;
     state.dirty = true;
   }
-};
-
-const draw = () => {
-  ctx.fillStyle = "rgb(200, 0, 0)";
-  ctx.fillRect(10, 10, 50, 50);
-
-  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-  ctx.fillRect(30, 30, 50, 50);
-};
-
-// ---------------------------
-
-const render = () => {
-  if (state.dirty) {
-    state.dirty = false;
-    draw();
+});
+canvas.addEventListener("mouseup", (e) => {
+  if (e.button === 0) {
+    state.mouse.pressed = false;
+    state.dirty = true;
   }
-
-  window.requestAnimationFrame(render);
-};
-
-window.requestAnimationFrame(render);
-window.addEventListener("resize", resizeCanvas, false);
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (state.mouse.pressed) {
+    state.mouse.posX = e.clientX;
+    state.mouse.posY = e.clientY;
+    state.dirty = true;
+  }
+});
