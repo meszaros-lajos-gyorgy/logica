b7a21b6a4f913d9924e841d3660ba69fa5848723
Mészáros Lajos <m_lajos@hotmail.com>
2021-03-19 19:53:07 +0100

    render state alapján + képernyő átméretezés figyelés

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-const screen = document.getElementById("screen");
+const canvas = document.getElementById("screen");
+const ctx = canvas.getContext("2d");
+
+const state = {
+  dirty: true,
+  screen: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+};
+
+const resizeCanvas = () => {
+  if (
+    state.screen.width !== window.innerWidth ||
+    state.screen.height !== window.innerHeight
+  ) {
+    state.screen.width = window.innerWidth;
+    state.screen.height = window.innerHeight;
+    state.dirty = true;
+  }
+};
+
+const draw = () => {
+  ctx.fillStyle = "rgb(200, 0, 0)";
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect(30, 30, 50, 50);
+};
+
+// ---------------------------
+
+const render = () => {
+  if (state.dirty) {
+    state.dirty = false;
+    draw();
+  }
+
+  window.requestAnimationFrame(render);
+};
+
+window.requestAnimationFrame(render);
+window.addEventListener("resize", resizeCanvas, false);
